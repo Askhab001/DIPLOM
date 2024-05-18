7d723a89f7a63e2031e2c14d598a694e876661e2
ashab <ashabttembul@gmail.com>
2024-05-18 18:37:20 +0600

    assa

--- main/admin.py
+++ main/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import LOL
 
 admin.site.register(LOL)
+
--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class LOL(models.Model):
     name = models.CharField('название команды', max_length=200)
     namecommand = models.CharField('Команда', max_length=200)
     game = models.CharField('Игра', max_length=200)
+
+
 
 
